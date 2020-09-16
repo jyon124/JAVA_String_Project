cbc0327ba0da96cf7e9746b739654f6ea0c2b0fa
Jooyon Won <jyon1204@gmail.com>
2020-09-16 12:11:50 -0700

    #Consonants Count & Replace method made

---
+++ src/com/sept/javastringproject/Consonants.java
@@ -0,0 +1,33 @@
+package com.sept.javastringproject;
+//  Count all consonants in a String and display the count
+//  Replace all consonants with '*' in a String
+
+public class Consonants {
+	public static String sentence = "count all consonants in a string";
+	
+	public int countConsonants(String s) {
+		
+		int count = 0;
+		
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z' &&
+			   s.charAt(i) != 'a' &&
+			   s.charAt(i) != 'i' &&
+			   s.charAt(i) != 'u' &&
+			   s.charAt(i) != 'e' &&
+			   s.charAt(i) != 'o') {
+				count+= 1;
+				s = s.substring(0, i) 
+			              + "*" 
+			              + s.substring(i + 1);
+			}
+		}
+		System.out.println(s);
+		return count;	
+		}
+
+	public static void main(String[] args) {
+		Consonants consonants = new Consonants();
+		System.out.println(consonants.countConsonants(sentence));
+	}
+}
